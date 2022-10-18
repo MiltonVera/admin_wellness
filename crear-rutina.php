346cdf6407b0e6e5da6c8af5a21ccf4ae26c3b2b
MiltonVera <a00833471@tec.mx>
2022-10-18 18:05:44 -0500

    rutina-crud

--- crear-rutina.php
+++ crear-rutina.php
@@ -96,6 +96,13 @@
                         </div>
 
                     </div>
+
+                    <div class="row">
+                        <div class="form-group col-md-12">
+                            <label for="repeticion">Descanso</label>
+                            <input type="number" class="form-control" name="descanso[]" min=0 max=10 step=".5" placeholder="Minutos de Descanso">
+                        </div>
+                    </div>
                     <button class="btn btn-danger col-12" id="eliminar">Eliminar</button>
                 </div>
 
